117855bc9d8f1ef175d2f06d72f34722ec76b420
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-08-26 17:58:28 +0200

    geht

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,26 @@ use std::process::{Command, Stdio};
 
 fn main() {
 
-    let mut child = Command::new("zfs get all -rHp")
+    let mut child = Command::new("zfs")
+        .arg("get")
+        .arg("all")
+        .arg("-rHp")
         .stdout(Stdio::piped())
-        .spawn();
+        .spawn().unwrap();
 
-    let child_stdout = child.stdout.as_mut(); // .unwrap();
+    let mut child_stdout = child.stdout.as_mut().unwrap();
 
-    let read_buffer = String::new();
+    let mut read_buffer = String::new();
 
     let read_res = child_stdout.read_to_string(&mut read_buffer);
 
-    // Close stdin to finish and avoid indefinite blocking
-    drop(child_stdout);
+    // // Close stdin to finish and avoid indefinite blocking
+    // drop(child_stdout);
 
     let output = child.wait_with_output();
 
+    println!("Yay!");
     println!("output = {:?}", output);
-    println!("length = {:?}", read_buffer.bytes())
-
-    Ok(())
+    println!("length = {:?}", read_buffer.bytes());
 
 }

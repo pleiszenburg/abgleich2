f63c1faa8f10e0127aaefd1e85839d5eb6744e30
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-10-03 16:07:22 +0200

    cli sub mod

---
+++ src/cli.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+
+use crate::zpool::Zpool;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "abgleich")]
+#[command(about = "abgleich, zfs sync tool", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// show dataset tree
+    #[command(arg_required_else_help = true)]
+    Tree {
+        /// configuration file
+        #[arg(required = true)]
+        config: PathBuf,
+    }
+}
+
+pub fn cli() {
+
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Tree { config } => {
+
+            let zpool = Zpool::from_local();
+            zpool.print_tree();
+
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod cli;
 mod dataset;
 mod datasettype;
 mod misc;
@@ -8,37 +9,8 @@ mod stat;
 mod table;
 mod zpool;
 
-use std::path::PathBuf;
-
-use crate::zpool::Zpool;
-use clap::{Parser, Subcommand};
-
-#[derive(Debug, Parser)] // requires `derive` feature
-#[command(name = "abgleich")]
-#[command(about = "abgleich, zfs sync tool", long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    /// show dataset tree
-    #[command(arg_required_else_help = true)]
-    Tree {
-        /// configuration file
-        #[arg(required = true)]
-        config: PathBuf,
-    }
-}
+use cli::cli;
 
 fn main() {
-    let args = Cli::parse();
-    match args.command {
-        Commands::Tree { config } => {
-            println!("Tree! {}", config.to_str().unwrap());
-            let zpool = Zpool::from_local();
-            zpool.print_tree();
-        }
-    }
+    cli();
 }

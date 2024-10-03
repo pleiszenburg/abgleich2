6bed77d699bc79c0c363742555a30d48d542aa87
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-10-03 19:25:23 +0200

    document parent

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -2,14 +2,16 @@ use crate::meta::Meta;
 
 pub struct Snapshot {
 
+    pub parent: String,
     pub meta: Meta,
 
 }
 
 impl Snapshot {
 
-    pub fn new (meta: Meta) -> Self {
+    pub fn new (parent: String, meta: Meta) -> Self {
         Self {
+            parent: parent,
             meta: meta,
         }
     }

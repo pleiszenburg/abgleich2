05f68a422cd10c3d3778ca6e10512eae418ded4e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-10-03 15:29:39 +0200

    first attempt

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,37 @@ mod stat;
 mod table;
 mod zpool;
 
+use std::path::PathBuf;
+
 use crate::zpool::Zpool;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "abgleich")]
+#[command(about = "abgleich, zfs sync tool", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// show dataset tree
+    #[command(arg_required_else_help = true)]
+    Tree {
+        /// configuration file
+        #[arg(required = true)]
+        config: PathBuf,
+    }
+}
 
 fn main() {
-    let zpool = Zpool::from_local();
-    zpool.print_tree();
+    let args = Cli::parse();
+    match args.command {
+        Commands::Tree { config } => {
+            println!("Tree! {}", config.to_str().unwrap());
+            let zpool = Zpool::from_local();
+            zpool.print_tree();
+        }
+    }
 }

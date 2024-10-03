d4c13eee9c2ba8442ffb1de26666a265faf2c483
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-10-03 19:25:07 +0200

    fix enum

--- src/datasettype.rs
+++ src/datasettype.rs
@@ -1,12 +1,14 @@
-#[derive(PartialEq)]
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub enum DatasetType {
+
     Filesystem,
     Volume,
     Snapshot,
+
 }
 
 impl DatasetType {
+
     pub fn from_raw(raw: &str) -> Self {
         match raw {
             "filesystem" => {
@@ -16,11 +18,29 @@ impl DatasetType {
                 Self::Volume
             }
             "snapshot" => {
-                Self::Volume
+                Self::Snapshot
             }
             _ => {
                 panic!("expected dataset type");
             }
         }
     }
+
+    // pub fn to_string(&self) -> String {
+    //     match &self {
+    //         Self::Filesystem => {
+    //             "filesystem".to_string()
+    //         }
+    //         Self::Volume => {
+    //             "volume".to_string()
+    //         }
+    //         Self::Snapshot => {
+    //             "snapshot".to_string()
+    //         }
+    //         _ => {
+    //             panic!("expected dataset type");
+    //         }
+    //     }
+    // }
+
 }

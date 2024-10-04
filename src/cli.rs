edbe1b8eb6ab6355c1c837676dc3c59cf6e50081
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-10-04 13:01:41 +0200

    prep snap command

--- src/cli.rs
+++ src/cli.rs
@@ -21,10 +21,22 @@ enum Commands {
     /// show dataset tree
     #[command(arg_required_else_help = true)]
     Tree {
+
         /// configuration file
         #[arg(required = true)]
         config: PathBuf,
-    }
+
+    },
+
+    /// create snapshots of changed datasets for backups
+    #[command(arg_required_else_help = true)]
+    Snap {
+
+        /// configuration file
+        #[arg(required = true)]
+        config: PathBuf,
+
+    },
 
 }
 
@@ -33,6 +45,7 @@ pub fn cli() {
     let args = Cli::parse();
 
     match args.command {
+
         Commands::Tree { config } => {
 
             let settings = Settings::from_configfile(config.to_str().unwrap());
@@ -40,7 +53,17 @@ pub fn cli() {
             let zpool = Zpool::from_local(&settings.source.root);
             zpool.print_tree();
 
-        }
+        },
+
+        Commands::Snap { config } => {
+
+            let settings = Settings::from_configfile(config.to_str().unwrap());
+
+            let zpool = Zpool::from_local(&settings.source.root);
+            zpool.print_tree();
+
+        },
+
     }
 
 }

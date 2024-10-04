c3ef004445586991a278b8a4a8ed667f6c185462
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-10-04 16:03:50 +0200

    snapshot change detection prototype

--- src/dataset.rs
+++ src/dataset.rs
@@ -1,3 +1,4 @@
+use crate::datasettype::DatasetType;
 use crate::meta::Meta;
 use crate::snapshot::Snapshot;
 
@@ -19,6 +20,45 @@ impl Dataset {
 
     pub fn add_snapshot(&mut self, snapshot: Snapshot) {
         self.snapshots.push(snapshot);
+    }
+
+    pub fn is_changed(&self, always_changed: bool, written_threshold: Option<u64>, check_diff: bool) -> bool {
+
+        if always_changed {
+            return true;
+        }
+        if self.snapshots.len() == 0 {
+            return true;
+        }
+        if self.meta.written.value.unwrap() == 0 {
+            return false;
+        }
+        if *self.meta.datasettype.value.as_ref().unwrap() == DatasetType::Volume {
+            return true;
+        }
+        match written_threshold {
+            Some(value) => {
+                if value > self.meta.written.value.unwrap() {
+                    return true;
+                }
+            },
+            _ => {},
+        }
+        if !check_diff {
+            return true;
+        }
+
+        // output, _ = (
+        //     Command.from_list(
+        //         ["zfs", "diff", f"{self._name:s}@{self._snapshots[-1].name:s}"]
+        //     )
+        //     .on_side(side=self._side, config=self._config)
+        //     .run()
+        // )
+        // return len(output[0].strip(" \t\n")) > 0
+
+        false  // TODO
+
     }
 
 }
